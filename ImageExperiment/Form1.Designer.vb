8d2940b1ab11b056c258cc8dc9a9cc043e2a0307
Trashman3573 <Trashman3573@gmail.com>
2020-06-08 11:00:13 +1000

    Initial commit

---
+++ ImageExperiment/Form1.Designer.vb
@@ -0,0 +1,156 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.NumericUpDown1 = New System.Windows.Forms.NumericUpDown()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.NumericUpDown2 = New System.Windows.Forms.NumericUpDown()
+        Me.Label2 = New System.Windows.Forms.Label()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.NumericUpDown2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 466)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Directory..."
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.PictureBox1.Location = New System.Drawing.Point(12, 12)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(661, 448)
+        Me.PictureBox1.TabIndex = 2
+        Me.PictureBox1.TabStop = False
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(93, 466)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 3
+        Me.Button2.Text = "Start"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Timer1
+        '
+        '
+        'NumericUpDown1
+        '
+        Me.NumericUpDown1.Increment = New Decimal(New Integer() {500, 0, 0, 0})
+        Me.NumericUpDown1.Location = New System.Drawing.Point(254, 469)
+        Me.NumericUpDown1.Maximum = New Decimal(New Integer() {3600000, 0, 0, 0})
+        Me.NumericUpDown1.Minimum = New Decimal(New Integer() {1, 0, 0, 0})
+        Me.NumericUpDown1.Name = "NumericUpDown1"
+        Me.NumericUpDown1.Size = New System.Drawing.Size(68, 20)
+        Me.NumericUpDown1.TabIndex = 5
+        Me.NumericUpDown1.Value = New Decimal(New Integer() {1000, 0, 0, 0})
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(557, 471)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(0, 13)
+        Me.Label1.TabIndex = 6
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(351, 470)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(83, 17)
+        Me.CheckBox1.TabIndex = 7
+        Me.CheckBox1.Text = "Transparent"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'NumericUpDown2
+        '
+        Me.NumericUpDown2.Enabled = False
+        Me.NumericUpDown2.Increment = New Decimal(New Integer() {5, 0, 0, 0})
+        Me.NumericUpDown2.Location = New System.Drawing.Point(451, 469)
+        Me.NumericUpDown2.Name = "NumericUpDown2"
+        Me.NumericUpDown2.Size = New System.Drawing.Size(44, 20)
+        Me.NumericUpDown2.TabIndex = 8
+        Me.NumericUpDown2.Value = New Decimal(New Integer() {30, 0, 0, 0})
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(211, 471)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(37, 13)
+        Me.Label2.TabIndex = 9
+        Me.Label2.Text = "Delay:"
+        Me.Label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(685, 495)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.NumericUpDown2)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.NumericUpDown1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.KeyPreview = True
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide
+        Me.Text = "Hypnosis Slideshow"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.NumericUpDown2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As Button
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents NumericUpDown1 As NumericUpDown
+    Friend WithEvents Label1 As Label
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents NumericUpDown2 As NumericUpDown
+    Friend WithEvents Label2 As Label
+End Class
---
+++ ImageExperiment/Form1.vb
@@ -0,0 +1,264 @@
+﻿Imports System.IO
+Imports System.Runtime.InteropServices
+Imports System.Text.RegularExpressions
+
+
+Public Class Form1
+
+    <DllImport("user32.dll", EntryPoint:="GetWindowLong")> Public Shared Function GetWindowLong(ByVal hWnd As IntPtr, ByVal nIndex As Integer) As Integer
+    End Function
+    <DllImport("user32.dll", EntryPoint:="SetWindowLong")> Public Shared Function SetWindowLong(ByVal hWnd As IntPtr, ByVal nIndex As Integer, ByVal dwNewLong As Integer) As Integer
+    End Function
+    <DllImport("user32.dll", EntryPoint:="SetLayeredWindowAttributes")> Public Shared Function SetLayeredWindowAttributes(ByVal hWnd As IntPtr, ByVal crKey As Integer, ByVal alpha As Byte, ByVal dwFlags As Integer) As Boolean
+    End Function
+
+    Dim Wdirectory As String
+    Dim speed As Integer
+    Dim transparency As Short
+
+
+    Dim imgType() As String = {"*.jpg", "*.png", "*.gif"}
+    Dim flatList As List(Of String) = New List(Of String)
+    Dim imgList As List(Of String) = New List(Of String)
+
+    Dim imgIndex As Integer = 0
+    Dim Paused As Boolean = True
+
+    Dim FullscreenModeBool As Boolean = False
+
+    Dim screen As Screen
+
+    Private InitialStyle As Integer
+    Dim PercentVisible As Decimal
+
+    Private Sub Init()
+        Wdirectory = My.Settings.DeafultDir
+        speed = My.Settings.DefaultSpeed
+        transparency = My.Settings.DefaultTra
+        Button2.Enabled = False
+        PictureBox1.SizeMode = PictureBoxSizeMode.Zoom
+        NumericUpDown2.Minimum = 1
+        NumericUpDown2.Maximum = 100
+        NumericUpDown2.Value = transparency
+        NumericUpDown1.Value = speed 
+
+        Randomize()
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Init()
+        Try
+            PicInit()
+        Catch ex As Exception
+
+        End Try
+
+    End Sub
+
+    Private Sub PicInit()
+        If Wdirectory <> "" Or Wdirectory <> " " Then
+            ImgInit(Wdirectory)
+            If flatList.Count > 0 Then
+
+                listShuffle()
+                Button2.Enabled = True
+                PictureBox1.ImageLocation = imgList.Item(imgIndex)
+                Label1.Text = imgIndex + 1.ToString & " / " & imgList.Count + 1
+            End If
+        End If
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        playToggle("stop")
+
+        Dim dirBrowser As FolderBrowserDialog = New FolderBrowserDialog()
+        dirBrowser.ShowDialog()
+        If dirBrowser.SelectedPath <> "" Then
+            Wdirectory = dirBrowser.SelectedPath
+            My.Settings.DeafultDir = dirBrowser.SelectedPath
+        End If
+
+        PicInit()
+
+    End Sub
+
+    Private Sub ImgInit(path As String)
+
+        flatList.Clear()
+        For Each ext As String In imgType
+            Dim a As String() = Directory.GetFiles(path, ext, SearchOption.AllDirectories)
+            For Each img As String In a
+                flatList.Add(img)
+            Next
+        Next
+    End Sub
+
+    Private Sub listShuffle()
+        Dim temp As List(Of String) = New List(Of String)
+        For Each img As String In flatList
+            temp.Add(img)
+        Next
+        imgList.Clear()
+        imgIndex = 0
+
+        For i = 0 To temp.Count - 1 Step 1
+            Randomize()
+            Dim j As Integer = CInt(Math.Floor((temp.Count() - 1 - 0 + 1) * Rnd())) + 0
+            imgList.Add(temp.Item(j).ToString)
+            temp.RemoveAt(j)
+        Next
+
+    End Sub
+
+    Dim preLoc As Point
+    Dim preSize As Size
+    Dim pPreSize As Size
+
+    Private Sub Slide(direction As Boolean)
+        If direction = True Then
+            imgIndex += 1
+        ElseIf direction = False Then
+            If imgIndex > 0 Then
+                imgIndex -= 1
+            End If
+        End If
+        PictureBox1.ImageLocation = imgList.Item(imgIndex)
+        Label1.Text = imgIndex + 1.ToString & " / " & imgList.Count + 1
+    End Sub
+
+    Private Sub FullScreenMode(state As Boolean)
+        screen = Screen.FromControl(Me)
+        Dim W As Integer = screen.Bounds.Width
+        Dim H As Integer = screen.Bounds.Height
+        If state = True Then
+
+
+            preLoc = Me.Location
+            preSize = Me.Size
+            pPreSize = PictureBox1.Size
+
+            Button1.Hide()
+            Button2.Hide()
+
+            PictureBox1.BorderStyle = BorderStyle.None
+            NumericUpDown1.Hide()
+            NumericUpDown2.Hide()
+            Label1.Hide()
+            CheckBox1.Hide()
+
+            Me.FormBorderStyle = FormBorderStyle.None
+            Me.BackColor = Color.Black
+            Me.Size = New Size(W, H)
+            Me.Location = New Point(screen.Bounds.X, screen.Bounds.Y)
+
+            PictureBox1.Location = New Point(0, 0)
+            PictureBox1.Size = New Size(W, H)
+
+            If CheckBox1.Checked = True Then
+                InitialStyle = GetWindowLong(Me.Handle, -20)
+                PercentVisible = transparency / 100
+                SetWindowLong(Me.Handle, -20, InitialStyle Or &H80000 Or &H20)
+                SetLayeredWindowAttributes(Me.Handle, 0, 255 * PercentVisible, &H2)
+                Me.TopMost = True
+            End If
+            FullscreenModeBool = True
+        ElseIf state = False Then
+            Button1.Show()
+            Button2.Show()
+
+            PictureBox1.BorderStyle = BorderStyle.FixedSingle
+            NumericUpDown1.Show()
+            NumericUpDown2.Show()
+            Label1.Show()
+            CheckBox1.Show()
+
+            Me.FormBorderStyle = FormBorderStyle.FixedSingle
+            Me.BackColor = SystemColors.Control
+            Me.Size = preSize
+            Me.Location = preLoc
+
+            PictureBox1.Location = New Point(5, 5)
+            PictureBox1.Size = pPreSize
+            FullscreenModeBool = False
+        End If
+    End Sub
+
+    Private Sub playToggle(Optional ByVal state As String = "toggle")
+        If state = "stop" Then
+            Timer1.Stop()
+            Button2.Text = "Start"
+            Paused = True
+        ElseIf state = "start" Then
+            speed = NumericUpDown1.Value
+            Timer1.Interval = speed
+            Timer1.Start()
+            Button2.Text = "Stop"
+            Paused = False
+        ElseIf state = "toggle" Then
+            If Paused = True Then
+                speed = NumericUpDown1.Value
+                Timer1.Interval = speed
+                Timer1.Start()
+                Button2.Text = "Stop"
+                Paused = False
+            Else
+                Timer1.Stop()
+                Button2.Text = "Start"
+                Paused = True
+            End If
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        FullScreenMode(True)
+        playToggle("toggle")
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        If imgIndex > imgList.Count - 1 Then
+            listShuffle()
+            Exit Sub
+        End If
+        Slide(True)
+    End Sub
+
+    Private Sub PictureBox1_DoubleClick(sender As Object, e As EventArgs) Handles PictureBox1.DoubleClick
+        playToggle("stop")
+        FullScreenMode(False)
+    End Sub
+
+    Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox1.CheckedChanged
+        If CheckBox1.Checked = True Then
+            CheckBox1.Text = "Transparency %:"
+            NumericUpDown2.Enabled = True
+        ElseIf CheckBox1.Checked = False Then
+            CheckBox1.Text = "Transparent"
+            NumericUpDown2.Enabled = False
+        End If
+    End Sub
+
+    Private Sub Form1_KeyDown(sender As Object, e As KeyEventArgs) Handles Me.KeyDown
+        If e.KeyCode = Keys.Left Then
+            Slide(False)
+        End If
+        If e.KeyCode = Keys.Right Then
+            Slide(True)
+        End If
+        If e.KeyCode = Keys.Escape Then
+            If FullscreenModeBool = True Then
+                FullScreenMode(False)
+                playToggle("stop")
+            End If
+        End If
+    End Sub
+
+    Private Sub NumericUpDown2_ValueChanged(sender As Object, e As EventArgs) Handles NumericUpDown2.ValueChanged
+        transparency = NumericUpDown2.Value
+        My.Settings.DefaultTra = transparency
+    End Sub
+
+    Private Sub NumericUpDown1_ValueChanged(sender As Object, e As EventArgs) Handles NumericUpDown1.ValueChanged
+        speed = NumericUpDown1.Value
+        My.Settings.DefaultSpeed = speed
+    End Sub
+End Class
